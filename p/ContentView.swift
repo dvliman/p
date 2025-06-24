f2fe8c44c48e22fc40f0533b128cbd4423434786
David Liman <limanoit@gmail.com>
2025-06-24 15:29:19 +0700

    Initial Commit

---
+++ p/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  p
+//
+//  Created by David Liman on 6/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ p/pApp.swift
@@ -0,0 +1,17 @@
+//
+//  pApp.swift
+//  p
+//
+//  Created by David Liman on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

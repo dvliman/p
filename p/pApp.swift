5be213d6383cbe297c6f040f3c72e91deccfa42d
David Liman <limanoit@gmail.com>
2025-06-29 14:37:57 +0700

    firebase

--- p/pApp.swift
+++ p/pApp.swift
@@ -1,14 +1,19 @@
-//
-//  pApp.swift
-//  p
-//
-//  Created by David Liman on 6/24/25.
-//
-
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct pApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
